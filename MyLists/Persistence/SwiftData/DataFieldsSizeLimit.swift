48d584a7ac9590ccbd60d9623a4491cb5108b55a
Otavio Zabaleta <otaviokz@gmail.com>
2024-10-16 23:58:21 -0300

    - Moved SwiftData field size limitation to Data related struct

---
+++ MyLists/Persistence/SwiftData/DataFieldsSizeLimit.swift
@@ -0,0 +1,13 @@
+//
+//  DataFieldsSizeLimit.swift
+//  ReusableLists
+//
+//  Created by Otávio Zabaleta on 16/10/2024.
+//
+
+import Foundation
+
+struct DataFieldsSizeLimit {
+    static var name: Int { 64 }
+    static var details: Int { 128 }
+}

01098281705756a52fe55641fe3b63a790b7e586
Otavio Zabaleta <otaviokz@gmail.com>
2024-10-29 22:25:03 -0300

    Version bump

--- MyLists/View/About/AboutView.swift
+++ MyLists/View/About/AboutView.swift
@@ -30,7 +30,7 @@ struct AboutView: View {
                     Label("Share", systemImage: "square.and.arrow.up")
                 }
                 
-                LabeledContent("Version", value: "2.3.1")
+                LabeledContent("Version", value: "2.4.0")
                 
                 HTMLView(fileName: "PrivacyPolicy")
                     .frame(height: UIScreen.main.bounds.height * 0.7)
